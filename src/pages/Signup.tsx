edfefb833bfe6403bd1002d1ef45ddb2da31fd04
Awino53 <estherojul53@gmail.com>
2026-04-09 05:20:08 +0300

    update API URL for production

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -7,7 +7,7 @@ const Signup = () => {
   const navigate = useNavigate();
 
   const handleSignup = async () => {
-    const res = await fetch("http://localhost:5000/api/auth/signup", {
+    const res = await fetch("https://jobtrackr-remote-dash.onrender.com/api/auth/signup", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
